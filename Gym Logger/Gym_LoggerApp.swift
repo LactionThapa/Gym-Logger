fe90e97fb10f9c1d47f188e556ef7b8f0c0ffbbf
LactionThapa <lactionthapa@gmail.com>
2025-07-11 12:54:52 +0100

    Initial Commit

---
+++ Gym Logger/Gym_LoggerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gym_LoggerApp.swift
+//  Gym Logger
+//
+//  Created by Laction Thapa on 11/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Gym_LoggerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gym LoggerTests/Gym_LoggerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Gym_LoggerTests.swift
+//  Gym LoggerTests
+//
+//  Created by Laction Thapa on 11/07/2025.
+//
+
+import Testing
+
+struct Gym_LoggerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
